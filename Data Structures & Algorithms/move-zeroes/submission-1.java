ff73cdb1ed0e2cca19855b9a4c8cccc3368c5d13
dharanishkathir <74916656+dharanishkathir@users.noreply.github.com>
2026-06-01 00:10:53 +0530

    Add: move-zeroes - submission-1

---
+++ Data Structures & Algorithms/move-zeroes/submission-1.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        
+        int i = 0 , j = 1;
+        for(; j < nums.length && i < nums.length; i++)
+        {
+            if(nums[i] == 0)
+            {
+                while(j < nums.length && nums[j] == 0)
+                {
+                    j++;
+                }
+                if(j < nums.length && nums[j] != 0)
+                {
+                    int t = nums[j];
+                    nums[j] = nums[i];
+                    nums[i] = t;
+                }
+            }
+            j++;
+        }
+    }
+}
