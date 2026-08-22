be2136225f53e49faf05f663b8ac352bc5f2752f
dharanishkathir <74916656+dharanishkathir@users.noreply.github.com>
2026-08-22 15:42:05 +0530

    Add: search-2d-matrix - submission-11

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-11.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean searchMatrix(int[][] ar, int t) {
+        
+        int l = 0, r = ar.length-1, col = ar[0].length-1;
+        int m = 0;
+        while(l <= r)
+        {
+            m = l + (r - l)/2;
+            if(ar[m][col] < t)
+            {
+                l = m + 1;
+            }
+            else if(ar[m][0] > t)
+            {
+                r = m - 1;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        
+        return bs(ar[m], t, true);
+    }
+
+    private boolean bs(int []arr, int t, boolean flag)
+    {
+        int l = 0, r = arr.length - 1;
+        while(l <= r)
+        {
+            int mid = l + ((r - l)/2);
+            if(arr[mid] == t)
+                return true;
+            else if(arr[mid] > t)
+                r = mid - 1;
+            else
+                l = mid + 1;
+        }
+        return false;
+    }
+}
